e785739c1169f75794192cccdd773524917e7cb9
bohan12 <Wang670792079@gmail.com>
2018-07-17 12:17:31 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<iframe height="850" width="650" src="https://bot.dialogflow.com/87c0b6d0-6d74-4c32-8041-80b496bd0594"></iframe>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
