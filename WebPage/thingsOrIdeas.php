8d1acdc01b30096b6700c2301e275346d6b335fc
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-21 16:20:50 +1200

    Update thingsOrIdeas.php

--- WebPage/thingsOrIdeas.php
+++ WebPage/thingsOrIdeas.php
@@ -1,77 +1,83 @@
 <!DOCTYPE html>
 <html>
-<head>
-<link href="../CSS/cssquestionb.css" media="screen" rel="stylesheet" type="text/css"/>
- <style>
-.error {color: #FF0000;}
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link href="../CSS/cssquestionb.css" media="screen" rel="stylesheet" type="text/css"/>
+ 		<style>
+			.error {color: #FF0000;}
 
-.border {
- border: 2 px solid lightblue;
- padding: 10;
-  border-radius:50px 20px;
-}
-</style>
+			.border {
+				border: 2 px solid lightblue;
+				padding: 10;
+  				border-radius:50px 20px;
+			}
+		</style>
 
- <title>Second Page</title>
-</head>
-<div style="padding-left:150px; padding-top:50px;">
-<body>
-<img src="../images/STEM.png" alt="stem" style="position: absolute; top: 0px; left: 150px;">
-<h1>Second Page</h1>
-<?php
-echo "this is the second page";
- // define variables and set to empty values
-$preferErr = "";
-$prefer = "";
+ 		<title>Second Page</title>
+	</head>
+	<div style="padding-left:150px; padding-top:50px;">
+		<body>
+			<img src="../images/STEM.png" alt="stem" style="position: absolute; top: 0px; left: 150px;">
+			<h1>Second Page</h1>
+			<?php
+			echo "this is the second page";
+ 			// define variables and set to empty values
+			$preferErr = "";
+			$prefer = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
   
-  if (empty($_POST["prefer"])) {
-    $preferErr = "required to choice one of the options";
-  } else {
-    $prefer = test_input($_POST["prefer"]);
-  }
-}
+  				if (empty($_POST["prefer"])) {
+  			  		$preferErr = "required to choice one of the options";
+  				}
+				else 
+				{
+    				$prefer = test_input($_POST["prefer"]);
+  				}
+			}
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?> 
-<p><a href="third.php">third page </a> <br /> - Went to third page</p>
+			function test_input($data) {
+  			$data = trim($data);
+ 			$data = stripslashes($data);
+  			$data = htmlspecialchars($data);
+ 			return $data;
+			}
+			?> 
+			
+			<p><a href="third.php">third page </a> <br /> - Went to third page</p>
 
-<form action="index.html">
-<button type="submit" formaction = "../index.php">index page</button>
-</form>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+			<form action="index.html">
+			<button type="submit" formaction = "../index.php">index page</button>
+			</form>
+			<p><span class="error">* required field</span></p>
+			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   
-  Do you prefer to working with: <span class="error">* <?php echo $preferErr;?></span>
-	</br>
-  <input type="radio" name="prefer" <?php if (isset($prefer) && $prefer=="female") echo "checked";?> value="a thing">a thing
+  			Do you prefer to working with: <span class="error">* <?php echo $preferErr;?></span>
+				
+			</br>
+			
+  			<input type="radio" name="prefer" <?php if (isset($prefer) && $prefer=="female") echo "checked";?> value="a thing">a thing
   
-  <br></br>
-  <div class="border">
-  <p> computer ....................</p>
-  </div>
-  <input type="radio" name="prefer" <?php if (isset($prefer) && $prefer=="male") echo "checked";?> value="Ideas">Ideas
-  <div id="border">
-  <p> Ideas ........................</p>
-  </div>
+ 			<br></br>
+  			<div class="border">
+  			<p> computer ....................</p>
+  			</div>
+  			<input type="radio" name="prefer" <?php if (isset($prefer) && $prefer=="male") echo "checked";?> value="Ideas">Ideas
+  			<div id="border">
+  			<p> Ideas ........................</p>
+  			</div>
   
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
+  			<br><br>
+  			<input type="submit" name="submit" value="Submit">  
+		</form>
 
-<?php
-echo "<h2>Your Input:</h2>";
+		<?php
+		echo "<h2>Your Input:</h2>";
 
-echo $prefer;
-?>
+		echo $prefer;
+		?>
 
-</body>
-</div>
+		</body>
+	</div>
 </html>
