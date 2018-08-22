84de80cdb604dbbf3cfebb051503169ec6d23126
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-22 15:30:48 +1200

    Update stemSubjectPreff.php

--- WebPage/stemSubjectPreff.php
+++ WebPage/stemSubjectPreff.php
@@ -9,7 +9,7 @@
 
 	<div class="header2">
 		<form action="start.php">
-            	<button type="submit" formaction = "start.php" style="float: right; margin-top: 15px; padding: 16px 16px; width: 140px;" >Previous Page</button>
+            	<button type="submit" formaction = "start.php" style="float: right; margin-top: 10px; height: 50px; width: 140px;" >Previous Page</button>
         	</form>
 	</div>	
 	    
