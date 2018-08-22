7b4dd03ae5547c110052dfc8aaa579f2c928f846
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-22 15:05:08 +1200

    Create cssresult.php

---
+++ CSS/cssresult.php
@@ -0,0 +1,110 @@
+.header {
+  background-color: #7a5a10;
+  height: 70px;
+  float: right;
+}
+
+.header2 {
+  background-color: #9c7925;
+  height: 70px;
+  float: right;
+}
+
+h1 {
+	color: #000000;
+	font-family: Verdana;
+	font-size: 22px; 
+}
+
+h3 {
+	color: #000000;
+	font-family: Verdana;
+	font-size: 18px;
+}
+
+div {
+    color: #000000;
+}
+
+body {
+    background: #bc982c;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	margin-left: 0px;
+	margin-top: 0px;
+}
+
+.dropbtn {
+    border-radius: 4px;
+    background-color: #7a5a10;
+    color: #ffffff;
+    margin-top: 5px;
+    padding: 16px;
+    width: 110px;
+    font-size: 16px;
+    border: 8px;
+    cursor: pointer;
+}
+
+.dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+.dropdown-content {
+    color: #ffffff;
+    border-radius: 4px;
+    margin-top: 5px;
+    padding: 16px;
+    width: 350px;
+    height: 150px;
+    display: none;
+    background-color: #364145;
+    overflow: auto;
+    
+    z-index: 1;
+}
+
+.dropdown-content a {
+    color: #ffffff;
+    margin-top: 5px;
+    padding: 20px 20px;
+    text-decoration: none;
+    display: block;
+}
+
+.dropdown a:hover {background-color: #9c7925;}
+
+.show {display: block;}
+
+#total_votes
+{
+	font-size:30px;
+	color:#FE2E2E;
+	font-weight:bold;
+}
+
+.div p
+{
+	margin:0px;
+	font-size:20px;
+	text-align:left;
+	color:#E6E6E6;
+}
+
+p
+{
+	color: #ffffff;
+}
+
+button {
+    border-radius: 4px;
+    background-color: #7a5a10;
+    color: #ffffff;
+    margin-top: 5px;
+    padding: 16px;
+    width: 240px;
+    font-size: 16px;
+    border: 8px;
+    cursor: pointer;
+}
