ea964d7c9d92c58954daae8cf2436d133e4045ce
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-22 14:06:45 +1200

    Update third.php

--- WebPage/third.php
+++ WebPage/third.php
@@ -1,19 +1,30 @@
 <!DOCTYPE html>
 <html>
-<body>
-<head>
- <title>third Page</title>
-</head>
-<h1>thirdpage</h1>
-<?php
-echo "this is the third page";
-?> 
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link href="../CSS/cssquestionc.css" media="screen" rel="stylesheet" type="text/css"/>
+		
+		<div class="header">
+		</div>
 
-<p><a href="fourth.php">fourth page </a> <br /> - Went to fourth page</p>
+		<div class="header2">
+		</div>	
 
-<form action="second.php">
-<button type="submit" formaction = "second.php">second page</button>
-</form>
+        <title>Second Page</title>
+	</head>
+	<div style="padding-left:150px; padding-top:50px;">
+	<body>
+		<img src="../images/STEM.png" alt="stem" style="position: absolute; top: 0px; left: 150px;">
+		<?php
+            echo "this is the third page";
+        ?> 
 
-</body>
+        <p><a href="fourth.php">fourth page </a> <br /> - Went to fourth page</p>
+
+        <form action="second.php">
+            <button type="submit" formaction = "second.php">second page</button>
+        </form>
+
+	</body>
+	</div>
 </html>
