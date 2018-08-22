0c44b5eb3c860955c30a77f555da9369119f66c9
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-22 14:32:16 +1200

    Update fourth.php

--- WebPage/fourth.php
+++ WebPage/fourth.php
@@ -1,18 +1,29 @@
 <!DOCTYPE html>
 <html>
-<body>
-<head>
- <title>fourth Page</title>
-</head>
-<h1>fourth page</h1>
-<?php
-echo "this is the fourth page";
-?> 
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link href="../CSS/cssquestionc.css" media="screen" rel="stylesheet" type="text/css"/>
+		
+		<div class="header">
+		</div>
 
-<p><a href="fifth.php">fifth page </a> <br /> - Went to fifth page</p>
-<form action="third.php">
-<button type="submit" formaction = "third.php">third page</button>
-</form>
+		<div class="header2">
+		</div>	
 
-</body>
+        <title>Fourth Page</title>
+	</head>
+	<div style="padding-left:150px; padding-top:50px;">
+	<body>
+		<img src="../images/STEM.png" alt="stem" style="position: absolute; top: 0px; left: 150px;">
+
+        <form action="third.php">
+            <button type="submit" formaction = "third.php">Previous Page</button>
+        </form>
+	
+		<form action="fifth.php">
+			<button type="submit" formaction = "fifth.php">Continue to next page ►</button>
+		</form>
+
+	</body>
+	</div>
 </html>
