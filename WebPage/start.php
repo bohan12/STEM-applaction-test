4b51302bb5d39871d6ae58f16a13ca208a8c88a7
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-22 14:45:59 +1200

    Create start.php

---
+++ WebPage/start.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link href="../CSS/cssquestione.css" media="screen" rel="stylesheet" type="text/css"/>
+		
+		<div class="header">
+		</div>
+
+		<div class="header2">
+		</div>	
+
+    <title>Start Page</title>
+	</head>
+	<div style="padding-left:150px; padding-top:50px;">
+	<body>
+		<img src="../images/STEM.png" alt="stem" style="position: absolute; top: 0px; left: 150px;">
+	
+	    <form action="fourth.php">
+			<button type="submit" formaction = "result.php">Begin ►</button>
+		</form>
+
+	</body>
+	</div>
+</html>
