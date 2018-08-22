47841acda46f766f7c57aed2533f8eb2e58231d8
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-22 14:46:59 +1200

    Update start.php

--- WebPage/start.php
+++ WebPage/start.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<link href="../CSS/cssquestione.css" media="screen" rel="stylesheet" type="text/css"/>
+		<link href="../CSS/cssstart.css" media="screen" rel="stylesheet" type="text/css"/>
 		
 		<div class="header">
 		</div>
