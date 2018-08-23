6efd26b6c7a7d9af21747363d6b6153996010f01
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-23 20:01:50 +1200

    Update start.php

--- WebPage/start.php
+++ WebPage/start.php
@@ -2,6 +2,8 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Start Page</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<link href="../CSS/cssstart.css" media="screen" rel="stylesheet" type="text/css"/>
 		
 		<div class="header">
@@ -10,9 +12,8 @@
 		<div class="header2">
 		</div>	
 
-    <title>Start Page</title>
 	</head>
-	<div style="padding-left:150px; padding-top:50px;">
+	<div class="framedefinition">
 	<body>
 		<img src="../images/STEM.png" alt="stem" style="position: absolute; top: 0px; left: 150px;">
 	
