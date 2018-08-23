06c1390a0d4179d69f1ca838955a7e4e9a86ea8f
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-23 20:07:40 +1200

    Update thingsOrIdeas.php

--- WebPage/thingsOrIdeas.php
+++ WebPage/thingsOrIdeas.php
@@ -32,7 +32,7 @@
 	</head>
 	<div class="framedefinition">
 		<body>
-			<img src="../images/STEM.png" alt="stem" style="position: absolute; top: 0px; left: 150px;">
+			<img src="../images/STEM.png" alt="stem" class="logo">
 			<h1>Second Page</h1>
 			<?php
 			echo "this is the second page";
