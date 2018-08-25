4290f29f33454dc6633a0860eb7b6d34483b2ed0
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-25 18:19:49 +1200

    Update start.php

--- WebPage/start.php
+++ WebPage/start.php
@@ -4,7 +4,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Start Page</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<link href="../CSS/cssstart.css" media="screen" rel="stylesheet" type="text/css"/>
+		<link href="../CSS/csshome.css" media="screen" rel="stylesheet" type="text/css"/>
 		
 		<div class="header">
 		</div>
