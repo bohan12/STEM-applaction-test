2d868cf3c0f79692fc5b25e2d358a860ce5b04ea
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-25 18:36:14 +1200

    Update result.php

--- WebPage/result.php
+++ WebPage/result.php
@@ -21,7 +21,7 @@
 	</head>
 	<div class="framedefinition">
 	<body>
-		<img src="../images/STEM.png" alt="stem" class="logo">
+		<img src="../images/STEMblur.png" alt="stem" class="logo">
 
         <form action="fifth.php">
             <button type="submit" formaction = "fifth.php">Previous Page</button>
