8e8d87725b8bf9eb7325ba66400d51500483efe4
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-28 08:43:39 +1200

    Update thingsOrIdeas.php

--- WebPage/thingsOrIdeas.php
+++ WebPage/thingsOrIdeas.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<link href="../CSS/cssthingsideas.css" media="screen" rel="stylesheet" type="text/css"/>
 		
 		<div class="header">
