87b60e558b004550ed7e3e622606e182aae81002
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-08-28 17:54:26 +1200

    Update start.php

--- WebPage/start.php
+++ WebPage/start.php
@@ -49,8 +49,8 @@
 		}
 		function EngineeringDesc() {
 			var engineering = document.getElementById("ebuttonhome");
-    			if (engineering.value=="E") engineering.value = "Engineering at AUT covers both traditional aspects " +
-			"of Engineering such as Electrical Engineering and Mechanical Engineering as well as the" +
+    			if (engineering.value=="E") engineering.value = "Engineering at AUT covers both traditional majors " +
+			"such as Electrical Engineering and Mechanical Engineering as well as the " +
 			"hardware aspects of Technology";
     			else engineering.value = "E";
 		}
