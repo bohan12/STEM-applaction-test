46045a8bb596a8c3dbf0c6e5dbacc969a336ecd3
Jeremy ma <majunfeng01@outlook.com>
2018-08-29 19:46:49 +1200

    Update thingsOrIdeas.php

--- WebPage/thingsOrIdeas.php
+++ WebPage/thingsOrIdeas.php
@@ -45,7 +45,7 @@
 			Do you prefer to working with: 
 			<div id="ComputerDIV">
 		
-			<img onclick="showdes1();" id="image" src="../images/question2/computerO.png">
+			<img onclick="showdes1(); changeImage1();" id="image" src="../images/question2/computerO.png">
 			 </div>
 			
 			<div id="div2">
