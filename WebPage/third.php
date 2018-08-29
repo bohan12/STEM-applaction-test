0b2d648cdba1ce6c076aca10408d48bfaf57082c
Jeremy ma <majunfeng01@outlook.com>
2018-08-29 18:38:43 +1200

    Update third.php

--- WebPage/third.php
+++ WebPage/third.php
@@ -22,6 +22,52 @@
 	<div class="framedefinition">
 	<body>
 		<img src="../images/STEMblur.png" alt="stem" class="logo">
+		<?php
+			echo "this is the second page";
+ 			// define variables and set to empty values
+			$preferErr = "";
+			$prefer = "";
+
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+  
+  				if (empty($_POST["prefer"])) {
+  			  		$preferErr = "required to choice one of the options";
+  				}
+				else 
+				{
+    				$prefer = test_input($_POST["prefer"]);
+  				}
+			}
+
+			function test_input($data) {
+  			$data = trim($data);
+ 			$data = stripslashes($data);
+  			$data = htmlspecialchars($data);
+ 			return $data;
+			}
+			?> 
+		<p><span class="error">* required field</span></p>
+			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  
+  			Do you prefer to working with: <span class="error">* <?php echo $preferErr;?></span>
+				
+			</br>
+			
+  			<input type="radio" name="prefer" <?php if (isset($prefer) && $prefer=="female") echo "checked";?> value="Team">Team
+  
+ 			<br></br>
+  			<div class="border">
+  			<p> Team ....................</p>
+  			</div>
+  			<input type="radio" name="prefer" <?php if (isset($prefer) && $prefer=="male") echo "checked";?> value="Individual">Individual
+  			<div id="border">
+  			<p> Individual ........................</p>
+  			</div>
+  
+  			<br><br>
+  			<input type="submit" name="submit" value="Submit">  
+		</form>
 
         	<form action="second.php">
             		<button type="submit" formaction = "thingsOrIdeas.php">Previous Page</button>
