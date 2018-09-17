5c2ff9c584c72a23f680262557276c73167057bb
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-09-18 11:40:52 +1200

    Update thingsOrIdeas.php

--- WebPage/thingsOrIdeas.php
+++ WebPage/thingsOrIdeas.php
@@ -51,7 +51,7 @@
 			<img onclick="changeImage8()" id="image8" src="../images/question2/gearO.png">
 			<img onclick="changeImage9()" id="image9" src="../images/question2/periodicO.png">
 			</div>
-			<div id="desctext" class="bottomtext">
+			<div id="desctext">
 				<p>Select an option for its description</p>
 			</div>
 			<script type="text/javascript">
