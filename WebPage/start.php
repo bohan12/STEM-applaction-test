f396d823a227498a118b967493384a8b45f72465
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-09-25 14:07:54 +1200

    Update start.php

--- WebPage/start.php
+++ WebPage/start.php
@@ -45,8 +45,10 @@
     			if (technology.value=="T") technology.value = "Technology at AUT focuses on Analytics, " +
 			"Computational Intelligence, Computer Science, IT Service Science, Networks and Security, " +
 			"and Software Development";
-    			else technology.value = "T";
-			else document.getElementById("tbuttonhome").innerHTML = "<h1>T</h1>";
+    			else {
+				technology.value = "T";
+				document.getElementById("tbuttonhome").innerHTML = "<h1>T</h1>";
+			}
 		}
 		function EngineeringDesc() {
 			var engineering = document.getElementById("ebuttonhome");
