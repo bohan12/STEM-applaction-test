d6a3ee641f4db89d493ec00152bc43d241122863
Jeremy ma <majunfeng01@outlook.com>
2018-10-02 17:50:12 +1300

    Update third.php

--- WebPage/third.php
+++ WebPage/third.php
@@ -32,7 +32,9 @@
 				<div id="individualdesc">
 					Hover the mouse on the image for more information.
 				</div>
+				<div class="container1">
 				<img src="../images/question3/individual.png" alt="Avatar" class="image">
+				</div>
 				<div class="overlay">
 				<div class="text">This is the description for individual.</div>
 				</div>
