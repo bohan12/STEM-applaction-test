a358269a64d066d81b3f6dad90b6a0a3ea16224d
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-10-02 17:26:35 +1300

    Update stemSubjectPreff.php

--- WebPage/stemSubjectPreff.php
+++ WebPage/stemSubjectPreff.php
@@ -319,6 +319,36 @@ function SetCookies()
 	document.cookie = "chemistry= " + chemistryPage1;
 	document.cookie = "physics= " + physicsPage1;
 	document.cookie = "statistics= " + statisticsPage1;
+	
+	checkCookie();
+}
+
+function getCookie() 
+{
+	var name = "biology";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) 
+	{
+		var c = ca[i];
+        	while (c.charAt(0) == ' ')
+		{
+           		c = c.substring(1);
+       		}
+        	if (c.indexOf(name) == 0) 
+		{
+            		return c.substring(name.length, c.length);
+        	}
+   	}
+    	return "";
+}
+				
+function checkCookie() 
+{
+    	var user = getCookie();
+    	if (user != "") 
+	{
+        	alert("Welcome again " + user);
+    	}  
 }
 </script>
 
