e6f2e87e0f694fad03a55b59ec5a0479b1e743dd
Jeremy ma <majunfeng01@outlook.com>
2018-10-02 16:33:04 +1300

    Update third.php

--- WebPage/third.php
+++ WebPage/third.php
@@ -48,7 +48,7 @@
 		function changeOption1() {
     			var x = document.getElementById("myDIV");
     			if (x.innerHTML === "Do you prefer individual work?") {
-				if(num<1){
+				if(num<0){
 					++num;
         				x.innerHTML = "You prefer individual work.";
    				} else {
@@ -62,7 +62,7 @@
 		function changeOption2() {
     			var x = document.getElementById("mydiv");
     			if (x.innerHTML === "Do you prefer team work?") {
-				if(num<1){
+				if(num<0){
 					++num;
         				x.innerHTML = "You prefer team work.";
    				} else {
