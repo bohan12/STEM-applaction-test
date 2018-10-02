c522526fa8063ff40ecc8c9f9129274095975160
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-10-02 16:50:37 +1300

    Update stemSubjectPreff.php

--- WebPage/stemSubjectPreff.php
+++ WebPage/stemSubjectPreff.php
@@ -274,7 +274,7 @@ It has two major branches, differential calculus, and integral calculus; these t
 
 </br>
 
-<form action="thingsOrIdeas.php" onSubmit="JavaScript:SetCookies()>
+<form action="thingsOrIdeas.php" onSubmit="JavaScript:SetCookies()">
 	<button type="submit" formaction = "thingsOrIdeas.php" style="margin-bottom: 100px 0px">Continue to next page ►</button>
 </form>
 
