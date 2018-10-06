0ff09ec66e2d190cff05cb3cf651fadff38eee61
bohan12 <Wang670792079@gmail.com>
2018-10-06 17:43:03 +1300

    Update fourth.php

--- WebPage/fourth.php
+++ WebPage/fourth.php
@@ -29,14 +29,14 @@
 		<div class="container">
 			<div class="split left">
 				<h1><span>The Naturel</span></h1>
-				<image src="../images/question4/enviroment.png" style="width:850px;height:457px;>
+				<image src="../images/question4/enviroment.png" style="width:510px;height:274px;>
 				<form action="fifth.php">
 				<button type="submit" formaction="fifth.php">PREFER</button>
 				</form>		
 			</div>
 			<div class="split right">
 				<h1><span>The office</span></h1>
-				<image src="../images/question4/webwxgetmsgimg.jpg" style="width:850px;height:457px;>
+				<image src="../images/question4/webwxgetmsgimg.jpg" style="width:510px;height:274px;>
 				<form action="fifth.php">
 				<button type="submit" formaction="fifth.php">PREFER</button>
 				</form>	
