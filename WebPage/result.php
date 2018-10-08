93475fc270188be32d6d22aca63fee3d4a718a57
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-10-09 12:06:52 +1300

    Update result.php

--- WebPage/result.php
+++ WebPage/result.php
@@ -256,7 +256,7 @@
 			document.getElementById('result').innerHTML="<p>You should look into studying "
 			+ "Mathematics at AUT</p><p>Scores:</p><p>Science: " + scienceResult + "</p>" +
 			"<p>Technology: " + technologyResult + "</p><p>Engineering: " + engineeringResult + "</p>" +
-			"<p>Mathematics: " + mathematicsResult + "</p>";
+			"<p>Mathematics: " + mathematicsResult + "</p>" + mathematicsResult;
 		}
 	}
 </script>
