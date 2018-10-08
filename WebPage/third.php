131934aa314848b1d83e0a39dedbae71831d278a
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-10-08 14:24:18 +1300

    Update third.php

--- WebPage/third.php
+++ WebPage/third.php
@@ -7,7 +7,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Third Page</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<link href="../CSS/cssquestionc.css" media="screen" rel="stylesheet" type="text/css"/>
+		<link href="../CSS/cssquestionthird.css" media="screen" rel="stylesheet" type="text/css"/>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script type="text/javascript">
 			
