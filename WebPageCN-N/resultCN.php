d5bb6229dcc6111eb487509cc823359a64cc8650
Jeremy ma <majunfeng01@outlook.com>
2018-10-16 09:53:52 +1300

    Update resultCN.php

--- WebPageCN-N/resultCN.php
+++ WebPageCN-N/resultCN.php
@@ -12,8 +12,8 @@
 			.<form action="https://www.aut.ac.nz/">
             			<button type="submit" formaction = "https://www.aut.ac.nz/" class="buttontitle" >AUT Homepage</button>
         		</form>
-			<form action="start.php">
-            			<button type="submit" formaction = "start.php" class="buttontitle" >Start Page</button>
+			<form action="startCN.php">
+            			<button type="submit" formaction = "startCN.php" class="buttontitle" >起始页</button>
 			</form>
 		</div>	
 
