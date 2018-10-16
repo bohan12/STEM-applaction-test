d3b974515a996efda9796fb378306997f2c42561
bohan12 <Wang670792079@gmail.com>
2018-10-17 11:51:48 +1300

    Update startCN.php

--- WebPageCN-N/startCN.php
+++ WebPageCN-N/startCN.php
@@ -4,7 +4,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Start Page-CN</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<link href="../CSS/cssbegin2.css" media="screen" rel="stylesheet" type="text/css"/>
+		<link href="../CSS/cssbeginfile.css" media="screen" rel="stylesheet" type="text/css"/>
 		
 		<div class="header">
 		</div>
