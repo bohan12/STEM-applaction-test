a69409c8d0cd362a07fd164c5b5f5d22cc046014
bohan12 <Wang670792079@gmail.com>
2018-10-18 17:05:58 +1300

    add the media for all browser
    add the media for all browser

--- WebPage/fifth.php
+++ WebPage/fifth.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<link href="../CSS/cssquestione.css" media="screen" rel="stylesheet" type="text/css"/>
+		<link href="../CSS/cssquestione.css" media="screen" rel="stylesheet" type="text/css" media="all" />
 		<div class="header">
 		</div>
 		<div class="header2">
