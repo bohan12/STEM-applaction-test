4ea6c4c6b85dd902608525cba4efda7e2ada671a
Jeremy ma <majunfeng01@outlook.com>
2018-10-21 15:55:50 +1300

    Update third.php

--- WebPage/third.php
+++ WebPage/third.php
@@ -78,7 +78,7 @@
 				
 				
 					<div class="imageindividual">				
-					<img src="../images/question3/individual.jpg" alt="Avatar" style="width:80%;height:80%; object-fit:contain">
+					<img src="../images/question3/individual.jpg" alt="Avatar" style="width:60%;height:60%; object-fit:contain">
 					<div class="textindividual">
 					<p>You like to work by yourself, directed by yourself or by the
 						direction of someone else. This includes some 
@@ -94,7 +94,7 @@
 			<div class="split right">
 				<h1><span>Team work</span></h1>
 				<div class= "imageteam">
-					<img src="../images/question3/teamwork.jpg" alt="Avatar" style="width:100%;height:100%; object-fit:contain">
+					<img src="../images/question3/teamwork.jpg" alt="Avatar" style="width:60%;height:60%; object-fit:contain">
 				         <div class="textteam">	
 			       		 <p> You like to work with a team. Examples of this are software development and engineering projects. </p>
 					</div>
