a64ba23cff7f07a060b9351c51416288b9b16f3b
Jeremy ma <majunfeng01@outlook.com>
2018-10-23 10:14:50 +1300

    Update thirdCN.php

--- WebPageCN-N/thirdCN.php
+++ WebPageCN-N/thirdCN.php
@@ -60,7 +60,7 @@
             			<button type="submit" formaction = "https://www.aut.ac.nz/" class="buttontitle" >AUT Homepage</button>
         		</form>
 			<form action="start.php">
-            			<button type="submit" formaction = "start.php" class="buttontitle" >Start Page</button>
+            			<button type="submit" formaction = "startCN.php" class="buttontitle" >Start Page</button>
 			</form>
 		</div>	
 
