de5f5da43e9593b2b8b6df2979ecb39525116d37
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-10-30 10:48:50 +1300

    Update and rename thingsOrIdeas.php to stemPage2.php

--- WebPage/stemPage2.php
+++ WebPage/stemPage2.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<link href="../CSS/cssthingsideas.css" media="screen" rel="stylesheet" type="text/css" media="all" />
+		<link href="../CSS/stemPage2.css" media="screen" rel="stylesheet" type="text/css" media="all" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<div class="header">
 		</div>
