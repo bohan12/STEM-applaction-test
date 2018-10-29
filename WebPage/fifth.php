914d04f02f3b08eb0987771d2385c0f4decad22b
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-10-29 19:12:52 +1300

    Update fifth.php

--- WebPage/fifth.php
+++ WebPage/fifth.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<link href="../CSS/cssquestione.css" media="screen" rel="stylesheet" type="text/css" media="all" />
 		<div class="header">
 		</div>
