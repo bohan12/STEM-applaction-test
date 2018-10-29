31d9cc2309a983e1e4a62ddac6724eac6b8dda0d
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-10-30 10:50:42 +1300

    Update and rename result.php to stemPageResult.php

--- WebPage/stemPageResult.php
+++ WebPage/stemPageResult.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<link href="../CSS/cssresult.css" media="screen" rel="stylesheet" type="text/css" media="all" />
+		<link href="../CSS/stemPageResult.css" media="screen" rel="stylesheet" type="text/css" media="all" />
 		
 		<div class="header">
 		</div>
