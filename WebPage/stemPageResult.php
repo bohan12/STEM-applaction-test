4babb216f9b2d75c28d953d872585d1e2c8782c6
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-10-30 17:26:07 +1300

    Update stemPageResult.php

--- WebPage/stemPageResult.php
+++ WebPage/stemPageResult.php
@@ -137,7 +137,7 @@
 	
 	//PAGE 2
 	
-	technologyResult = technologyResult + computerScore*6;
+	technologyResult = technologyResult + computerScore*12;
 	mathematicsResult = mathematicsResult + pythagorasScore*4;
 	engineeringResult = mathematicsResult + pythagorasScore*2;
 	scienceResult = scienceResult + cellScore*6;
@@ -162,14 +162,14 @@
 	//PAGE 4
 	scienceResult = scienceResult + outsideScore*5;
 	engineeringResult = engineeringResult + outsideScore*10;
-	scienceResult = scienceResult + insideScore*10;
+	scienceResult = scienceResult + insideScore*5;
 	engineeringResult = engineeringResult + insideScore*5;
 	mathematicsResult = mathematicsResult + insideScore*10;
 	technologyResult = technologyResult + insideScore*10;
 	
 	//PAGE 5
 	technologyResult = technologyResult + creativeScore*10;
-	scienceResult = scienceResult + creativeScore*10;
+	scienceResult = scienceResult * (creativeScore*20.53);
 	mathematicsResult = mathematicsResult + convergentScore*10;
 	engineeringResult = engineeringResult + convergentScore*10;
 	
